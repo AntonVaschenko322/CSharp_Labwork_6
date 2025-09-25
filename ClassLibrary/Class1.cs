1101586a3abf0e5f071ad7384ef6c06ce3d3fdc2
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-09-25 23:31:33 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClassLibrary
+{
+    public class Weather
+    {
+        //[JsonPropertyName("main")]
+        //public string Main { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+    }
+
+    public class Main
+    {
+        [JsonPropertyName("temp")]
+        public double Temp { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonPropertyName("country")]
+        public string country { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("weather")]
+        public Weather[] weather { get; set; }
+
+        [JsonPropertyName("main")]
+        public Main main { get; set; }
+
+        [JsonPropertyName("sys")]
+        public Sys sys { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+    }
+
+
+    public struct Weathers
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public double Temp { get; set; }
+        public string Description { get; set; }
+
+        public Weathers(string country, string city, double temp, string description)
+        {
+            Country = country;
+            City = city;
+            Temp = temp;
+            Description = description;
+            
+        }
+    }
+
+
+
+}
---
+++ Labwork_6/Program.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Labwork_6
+{
+    public class Program
+    {
+
+        static async Task Main()
+        {
+            double l_min = -180;
+            double l_max = 180;
+            double s_min = -90;
+            double s_max = 90;
+            Random rnd = new();
+            List<Weathers>dat = new();
+            using HttpClient cli = new HttpClient();
+            for (int i = 0; i < 50;)
+            {
+                double l_range = l_min + (rnd.NextDouble() * (l_max - l_min));
+                double s_range = s_min + (rnd.NextDouble() * (s_max - s_min));
+                HttpResponseMessage response = await cli.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={s_range}&lon={l_range}&appid=02259164e7b6038f60788609f6c8d84e");
+                string json = await response.Content.ReadAsStringAsync();
+                Data wes = JsonSerializer.Deserialize<Data>(json);
+                if (wes.Name == null || wes.sys.country == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    Weathers x = new Weathers(wes.sys.country, wes.Name, wes.main.Temp, wes.weather[0].Description);
+                    dat.Add(x);
+                    i++;
+                }
+            }
+
+            List<Weathers> TempCountrys = dat.OrderByDescending(x => x.Temp).ToList();
+            Weathers MaxHot = dat.MaxBy(x => x.Temp);
+            Weathers MaxCold = dat.MinBy(x => x.Temp);
+
+            Console.WriteLine($"Самая большая температура в { MaxHot.Country } {MaxHot.City} {MaxHot.Temp}");
+            Console.WriteLine($"Самая низкая температура в {MaxCold.Country}  {MaxCold.City} {MaxCold.Temp}");
+            var AverTemp = dat.Average(x => x.Temp);
+            Console.WriteLine($"Средняя температура по Миру: { AverTemp} ");
+
+            var CountryCount = dat.Select(x => x.Country).Distinct().Count();
+            Console.WriteLine(@"Всего стран: {CountryCount} ");
+
+            var FrstCountryCS = dat.Where(x => x.Description == "clear sky").FirstOrDefault();
+            Console.WriteLine($"Первая страна у которой чистое небо {FrstCountryCS.Country}");
+
+            var FrstCountryRain = dat.Where(x => x.Description == "rain").FirstOrDefault();
+            Console.WriteLine($"Первая страна у которой дождь {FrstCountryRain.Country}");
+
+            var FrstCountryFC = dat.Where(x => x.Description == "few clouds").FirstOrDefault();
+            Console.WriteLine($"Первая страна у которой облачно {FrstCountryFC.Country}");
+
+
+            foreach (var x in dat)
+            {
+                Console.WriteLine(x.Country + " " + x.City + " " + x.Temp + " " + x.Description);
+            }
+        }
+    }
+
+}
+
